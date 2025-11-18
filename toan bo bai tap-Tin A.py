31afd1996e57654aea2a452b11a6cfb834b0af3a
voducanhkhoi07-crypto <voducanhkhoi.07@gmail.com>
2025-11-18 12:30:40 +0700

    Add files via upload

---
+++ toan bo bai tap-Tin A.py
@@ -0,0 +1,802 @@
+# Full Solution Placeholder for all exercises
+
+# Bài 1.1
+def bai_1_1():
+    pass
+
+# Bài 1.2
+def bai_1_2():
+    pass
+
+# Bài 1.3
+def bai_1_3():
+    pass
+
+# Bài 1.4
+def bai_1_4():
+    pass
+
+# Bài 1.5
+def bai_1_5():
+    pass
+
+# Bài 1.6
+def bai_1_6():
+    pass
+
+# Bài 1.7
+def bai_1_7():
+    pass
+
+# Bài 1.8
+def bai_1_8():
+    pass
+
+# Bài 1.9
+def bai_1_9():
+    pass
+
+# Bài 1.10
+def bai_1_10():
+    pass
+
+# Bài 1.11
+def bai_1_11():
+    pass
+
+# Bài 1.12
+def bai_1_12():
+    pass
+
+# Bài 1.13
+def bai_1_13():
+    pass
+
+# Bài 1.14
+def bai_1_14():
+    pass
+
+# Bài 1.15
+def bai_1_15():
+    pass
+
+# Bài 1.16
+def bai_1_16():
+    pass
+
+# Bài 1.17
+def bai_1_17():
+    pass
+
+# Bài 1.18
+def bai_1_18():
+    pass
+
+# Bài 1.19
+def bai_1_19():
+    pass
+
+# Bài 1.20
+def bai_1_20():
+    pass
+
+# Bài 2.1
+def bai_2_1():
+    pass
+
+# Bài 2.2
+def bai_2_2():
+    pass
+
+# Bài 2.3
+def bai_2_3():
+    pass
+
+# Bài 2.4
+def bai_2_4():
+    pass
+
+# Bài 2.5
+def bai_2_5():
+    pass
+
+# Bài 2.6
+def bai_2_6():
+    pass
+
+# Bài 2.7
+def bai_2_7():
+    pass
+
+# Bài 2.8
+def bai_2_8():
+    pass
+
+# Bài 2.9
+def bai_2_9():
+    pass
+
+# Bài 2.10
+def bai_2_10():
+    pass
+
+# Bài 2.11
+def bai_2_11():
+    pass
+
+# Bài 2.12
+def bai_2_12():
+    pass
+
+# Bài 2.13
+def bai_2_13():
+    pass
+
+# Bài 2.14
+def bai_2_14():
+    pass
+
+# Bài 2.15
+def bai_2_15():
+    pass
+
+# Bài 2.16
+def bai_2_16():
+    pass
+
+# Bài 2.17
+def bai_2_17():
+    pass
+
+# Bài 2.18
+def bai_2_18():
+    pass
+
+# Bài 2.19
+def bai_2_19():
+    pass
+
+# Bài 2.20
+def bai_2_20():
+    pass
+
+# Bài 3.1
+def bai_3_1():
+    pass
+
+# Bài 3.2
+def bai_3_2():
+    pass
+
+# Bài 3.3
+def bai_3_3():
+    pass
+
+# Bài 3.4
+def bai_3_4():
+    pass
+
+# Bài 3.5
+def bai_3_5():
+    pass
+
+# Bài 3.6
+def bai_3_6():
+    pass
+
+# Bài 3.7
+def bai_3_7():
+    pass
+
+# Bài 3.8
+def bai_3_8():
+    pass
+
+# Bài 3.9
+def bai_3_9():
+    pass
+
+# Bài 3.10
+def bai_3_10():
+    pass
+
+# Bài 3.11
+def bai_3_11():
+    pass
+
+# Bài 3.12
+def bai_3_12():
+    pass
+
+# Bài 3.13
+def bai_3_13():
+    pass
+
+# Bài 3.14
+def bai_3_14():
+    pass
+
+# Bài 3.15
+def bai_3_15():
+    pass
+
+# Bài 3.16
+def bai_3_16():
+    pass
+
+# Bài 3.17
+def bai_3_17():
+    pass
+
+# Bài 3.18
+def bai_3_18():
+    pass
+
+# Bài 3.19
+def bai_3_19():
+    pass
+
+# Bài 3.20
+def bai_3_20():
+    pass
+
+# Bài 4.1
+def bai_4_1():
+    pass
+
+# Bài 4.2
+def bai_4_2():
+    pass
+
+# Bài 4.3
+def bai_4_3():
+    pass
+
+# Bài 4.4
+def bai_4_4():
+    pass
+
+# Bài 4.5
+def bai_4_5():
+    pass
+
+# Bài 4.6
+def bai_4_6():
+    pass
+
+# Bài 4.7
+def bai_4_7():
+    pass
+
+# Bài 4.8
+def bai_4_8():
+    pass
+
+# Bài 4.9
+def bai_4_9():
+    pass
+
+# Bài 4.10
+def bai_4_10():
+    pass
+
+# Bài 4.11
+def bai_4_11():
+    pass
+
+# Bài 4.12
+def bai_4_12():
+    pass
+
+# Bài 4.13
+def bai_4_13():
+    pass
+
+# Bài 4.14
+def bai_4_14():
+    pass
+
+# Bài 4.15
+def bai_4_15():
+    pass
+
+# Bài 4.16
+def bai_4_16():
+    pass
+
+# Bài 4.17
+def bai_4_17():
+    pass
+
+# Bài 4.18
+def bai_4_18():
+    pass
+
+# Bài 4.19
+def bai_4_19():
+    pass
+
+# Bài 4.20
+def bai_4_20():
+    pass
+
+# Bài 5.1
+def bai_5_1():
+    pass
+
+# Bài 5.2
+def bai_5_2():
+    pass
+
+# Bài 5.3
+def bai_5_3():
+    pass
+
+# Bài 5.4
+def bai_5_4():
+    pass
+
+# Bài 5.5
+def bai_5_5():
+    pass
+
+# Bài 5.6
+def bai_5_6():
+    pass
+
+# Bài 5.7
+def bai_5_7():
+    pass
+
+# Bài 5.8
+def bai_5_8():
+    pass
+
+# Bài 5.9
+def bai_5_9():
+    pass
+
+# Bài 5.10
+def bai_5_10():
+    pass
+
+# Bài 5.11
+def bai_5_11():
+    pass
+
+# Bài 5.12
+def bai_5_12():
+    pass
+
+# Bài 5.13
+def bai_5_13():
+    pass
+
+# Bài 5.14
+def bai_5_14():
+    pass
+
+# Bài 5.15
+def bai_5_15():
+    pass
+
+# Bài 5.16
+def bai_5_16():
+    pass
+
+# Bài 5.17
+def bai_5_17():
+    pass
+
+# Bài 5.18
+def bai_5_18():
+    pass
+
+# Bài 5.19
+def bai_5_19():
+    pass
+
+# Bài 5.20
+def bai_5_20():
+    pass
+
+# Bài 6.1
+def bai_6_1():
+    pass
+
+# Bài 6.2
+def bai_6_2():
+    pass
+
+# Bài 6.3
+def bai_6_3():
+    pass
+
+# Bài 6.4
+def bai_6_4():
+    pass
+
+# Bài 6.5
+def bai_6_5():
+    pass
+
+# Bài 6.6
+def bai_6_6():
+    pass
+
+# Bài 6.7
+def bai_6_7():
+    pass
+
+# Bài 6.8
+def bai_6_8():
+    pass
+
+# Bài 6.9
+def bai_6_9():
+    pass
+
+# Bài 6.10
+def bai_6_10():
+    pass
+
+# Bài 6.11
+def bai_6_11():
+    pass
+
+# Bài 6.12
+def bai_6_12():
+    pass
+
+# Bài 6.13
+def bai_6_13():
+    pass
+
+# Bài 6.14
+def bai_6_14():
+    pass
+
+# Bài 6.15
+def bai_6_15():
+    pass
+
+# Bài 6.16
+def bai_6_16():
+    pass
+
+# Bài 6.17
+def bai_6_17():
+    pass
+
+# Bài 6.18
+def bai_6_18():
+    pass
+
+# Bài 6.19
+def bai_6_19():
+    pass
+
+# Bài 6.20
+def bai_6_20():
+    pass
+
+# Bài 7.1
+def bai_7_1():
+    pass
+
+# Bài 7.2
+def bai_7_2():
+    pass
+
+# Bài 7.3
+def bai_7_3():
+    pass
+
+# Bài 7.4
+def bai_7_4():
+    pass
+
+# Bài 7.5
+def bai_7_5():
+    pass
+
+# Bài 7.6
+def bai_7_6():
+    pass
+
+# Bài 7.7
+def bai_7_7():
+    pass
+
+# Bài 7.8
+def bai_7_8():
+    pass
+
+# Bài 7.9
+def bai_7_9():
+    pass
+
+# Bài 7.10
+def bai_7_10():
+    pass
+
+# Bài 7.11
+def bai_7_11():
+    pass
+
+# Bài 7.12
+def bai_7_12():
+    pass
+
+# Bài 7.13
+def bai_7_13():
+    pass
+
+# Bài 7.14
+def bai_7_14():
+    pass
+
+# Bài 7.15
+def bai_7_15():
+    pass
+
+# Bài 7.16
+def bai_7_16():
+    pass
+
+# Bài 7.17
+def bai_7_17():
+    pass
+
+# Bài 7.18
+def bai_7_18():
+    pass
+
+# Bài 7.19
+def bai_7_19():
+    pass
+
+# Bài 7.20
+def bai_7_20():
+    pass
+
+# Bài 8.1
+def bai_8_1():
+    pass
+
+# Bài 8.2
+def bai_8_2():
+    pass
+
+# Bài 8.3
+def bai_8_3():
+    pass
+
+# Bài 8.4
+def bai_8_4():
+    pass
+
+# Bài 8.5
+def bai_8_5():
+    pass
+
+# Bài 8.6
+def bai_8_6():
+    pass
+
+# Bài 8.7
+def bai_8_7():
+    pass
+
+# Bài 8.8
+def bai_8_8():
+    pass
+
+# Bài 8.9
+def bai_8_9():
+    pass
+
+# Bài 8.10
+def bai_8_10():
+    pass
+
+# Bài 8.11
+def bai_8_11():
+    pass
+
+# Bài 8.12
+def bai_8_12():
+    pass
+
+# Bài 8.13
+def bai_8_13():
+    pass
+
+# Bài 8.14
+def bai_8_14():
+    pass
+
+# Bài 8.15
+def bai_8_15():
+    pass
+
+# Bài 8.16
+def bai_8_16():
+    pass
+
+# Bài 8.17
+def bai_8_17():
+    pass
+
+# Bài 8.18
+def bai_8_18():
+    pass
+
+# Bài 8.19
+def bai_8_19():
+    pass
+
+# Bài 8.20
+def bai_8_20():
+    pass
+
+# Bài 9.1
+def bai_9_1():
+    pass
+
+# Bài 9.2
+def bai_9_2():
+    pass
+
+# Bài 9.3
+def bai_9_3():
+    pass
+
+# Bài 9.4
+def bai_9_4():
+    pass
+
+# Bài 9.5
+def bai_9_5():
+    pass
+
+# Bài 9.6
+def bai_9_6():
+    pass
+
+# Bài 9.7
+def bai_9_7():
+    pass
+
+# Bài 9.8
+def bai_9_8():
+    pass
+
+# Bài 9.9
+def bai_9_9():
+    pass
+
+# Bài 9.10
+def bai_9_10():
+    pass
+
+# Bài 9.11
+def bai_9_11():
+    pass
+
+# Bài 9.12
+def bai_9_12():
+    pass
+
+# Bài 9.13
+def bai_9_13():
+    pass
+
+# Bài 9.14
+def bai_9_14():
+    pass
+
+# Bài 9.15
+def bai_9_15():
+    pass
+
+# Bài 9.16
+def bai_9_16():
+    pass
+
+# Bài 9.17
+def bai_9_17():
+    pass
+
+# Bài 9.18
+def bai_9_18():
+    pass
+
+# Bài 9.19
+def bai_9_19():
+    pass
+
+# Bài 9.20
+def bai_9_20():
+    pass
+
+# Bài 10.1
+def bai_10_1():
+    pass
+
+# Bài 10.2
+def bai_10_2():
+    pass
+
+# Bài 10.3
+def bai_10_3():
+    pass
+
+# Bài 10.4
+def bai_10_4():
+    pass
+
+# Bài 10.5
+def bai_10_5():
+    pass
+
+# Bài 10.6
+def bai_10_6():
+    pass
+
+# Bài 10.7
+def bai_10_7():
+    pass
+
+# Bài 10.8
+def bai_10_8():
+    pass
+
+# Bài 10.9
+def bai_10_9():
+    pass
+
+# Bài 10.10
+def bai_10_10():
+    pass
+
+# Bài 10.11
+def bai_10_11():
+    pass
+
+# Bài 10.12
+def bai_10_12():
+    pass
+
+# Bài 10.13
+def bai_10_13():
+    pass
+
+# Bài 10.14
+def bai_10_14():
+    pass
+
+# Bài 10.15
+def bai_10_15():
+    pass
+
+# Bài 10.16
+def bai_10_16():
+    pass
+
+# Bài 10.17
+def bai_10_17():
+    pass
+
+# Bài 10.18
+def bai_10_18():
+    pass
+
+# Bài 10.19
+def bai_10_19():
+    pass
+
+# Bài 10.20
+def bai_10_20():
+    pass
+
